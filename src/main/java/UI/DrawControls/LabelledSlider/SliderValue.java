80bc34395526b0a13a7e68572d9d205d7d534632
martinhsl68hw <mmholl@ic.ac.uk>
2023-11-06 16:28:09 +0000

    First commit

---
+++ src/main/java/UI/DrawControls/LabelledSlider/SliderValue.java
@@ -0,0 +1,9 @@
+package UI.DrawControls.LabelledSlider;
+
+public class SliderValue {
+    // UI.LabelledSliderPanel.SliderValue is a class that holds a single integer value and is mutable so that the value can be changed by the front end
+    public int value;
+    public SliderValue(int value) {
+        this.value=value;
+    }
+}
